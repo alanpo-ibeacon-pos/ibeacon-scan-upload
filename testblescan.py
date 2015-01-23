cd1084296cea3d6192d09367bfa995f0aae3cb2e
hkgsherlock <hkgsherlock@gmail.com>
2015-01-23 18:48:10 +0800

    10 -> 1

--- testblescan.py
+++ testblescan.py
@@ -19,7 +19,7 @@ blescan.hci_le_set_scan_parameters(sock)
 blescan.hci_enable_le_scan(sock)
 
 while True:
-        returnedList = blescan.parse_events(sock, 10)
+        returnedList = blescan.parse_events(sock, 1)
         print("----------")
         for beacon in returnedList:
                 print(beacon)
